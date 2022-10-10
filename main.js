e02a8b4c039347230ebefe841affd6b4efbf0b32
Kai Jukarainen <kai.jukarainen@gmail.com>
2022-10-10 11:28:17 +0300

    Adjusting

--- main.js
+++ main.js
@@ -6,13 +6,13 @@ let aple = [1, 5];
 let melon = [2, 6, 9];
 let pear = [3, 7, 10, 12];
 let cherry = [4, 8, 11, 13, 14];
-let rollerFrames = []
+let rollerFrames = [0, 0, 0, 0]
 
 //Random number generators//
-let random = Math.floor(Math.random(15) * 150)
-let random2 = Math.floor(Math.random(15) * 150)
-let random3 = Math.floor(Math.random(15) * 150)
-let random4 = Math.floor(Math.random(15) * 150)
+let random = Math.floor(Math.random(16) * 250)
+let random2 = Math.floor(Math.random(20) * 250)
+let random3 = Math.floor(Math.random(30) * 250)
+let random4 = Math.floor(Math.random(25) * 250)
 
 //Canvas//
 const canvas = document.getElementById('canvas');
@@ -62,9 +62,16 @@ let btn = document.getElementById('start').addEventListener('click',function (){
 
 //Rulla animation//
 function animate(){
+    if (x < 4){
     contex.clearRect(0, 0, canvasW, canvasH);
-    animateRollers()
+    animateRoller1()
+    animateRoller2()
+    animateRoller3()
+    animateRoller4()
     requestAnimationFrame(animate);
+    }else{
+        checkRollers()
+    }
 };
 //Lopeta animaatio//
 function end() {
@@ -72,73 +79,77 @@ function end() {
     console.log(roller1frame);
 }
 
-function animateRollers(){
+function animateRoller1(){
     contex.drawImage(roller1, 0 * spriteW, roller1frame * spriteH, spriteW, spriteH, 100, 250, spriteW, spriteH);
+    if(gameFrame < random){
+        if (gameFrame % staggerFrames == 0){
+            if (roller1frame < 14) roller1frame++;
+            else roller1frame = 0;
+        }
+        gameFrame++;
+        if (gameFrame === random){
+            cancelAnimationFrame(gameFrame);
+            console.log(roller1frame);
+            rollerFrames.push(roller1frame)
+            x++;
+        }
+    }
+}
+function animateRoller2(){
     contex.drawImage(roller1, 0 * spriteW, roller2frame * spriteH, spriteW, spriteH, 200, 250, spriteW, spriteH);
-    contex.drawImage(roller1, 0 * spriteW, roller3frame * spriteH, spriteW, spriteH, 300, 250, spriteW, spriteH);
-    contex.drawImage(roller1, 0 * spriteW, roller4frame * spriteH, spriteW, spriteH, 400, 250, spriteW, spriteH);
-    if(x < 4){
-        if(gameFrame < random){
-            if (gameFrame % staggerFrames == 0){
-                if (roller1frame < 15) roller1frame++;
-                else roller1frame = 0;
-            }
-            gameFrame++;
-            if (gameFrame === random){
-                cancelAnimationFrame(gameFrame);
-                console.log(roller1frame);
-                rollerFrames.push(roller1frame)
-                x++;
-            }
+    if(gameFrame2 < random2){
+        if (gameFrame2 % staggerFrames == 0){
+            if (roller2frame < 14) roller2frame++;
+            else roller2frame = 0;
         }
-        if(gameFrame2 < random2){
-            if (gameFrame2 % staggerFrames === 0){
-                if (roller2frame < 15) roller2frame++;
-                else roller2frame = 0;
-            }
-            gameFrame2++;
-            if (gameFrame2 == random2){
-                cancelAnimationFrame(gameFrame2);
-                console.log(roller2frame);
-                rollerFrames.push(roller2frame)
-                x++;
-            }
+        gameFrame2++;
+        if (gameFrame2 === random2){
+            cancelAnimationFrame(gameFrame2);
+            console.log(roller2frame);
+            rollerFrames.push(roller2frame)
+            x++;
         }
-        if(gameFrame < random3){
-            if (gameFrame3 % staggerFrames == 0){
-                if (roller3frame < 15) roller3frame++;
-                else roller3frame = 0;
-            }
-            gameFrame3++;
-            if (gameFrame3 === random3){
-                cancelAnimationFrame(gameFrame3);
-                console.log(roller3frame);
-                rollerFrames.push(roller3frame)
-                x++;
-            }
+    }
+}
+function animateRoller3(){
+    contex.drawImage(roller1, 0 * spriteW, roller3frame * spriteH, spriteW, spriteH, 400, 250, spriteW, spriteH);
+    if(gameFrame3 < random3){
+        if (gameFrame3 % staggerFrames == 0){
+            if (roller3frame < 14) roller3frame++;
+            else roller3frame = 0;
         }
-        if(gameFrame4 < random4){
-            if (gameFrame4 % staggerFrames === 0){
-                if (roller4frame < 15) roller4frame++;
-                else roller4frame = 0;
-            }
-            gameFrame4++;
-            if (gameFrame4 == random4){
-                cancelAnimationFrame(gameFrame4);
-                console.log(roller4frame);
-                rollerFrames.push(roller4frame)
-                x++;
-            }
+        gameFrame3++;
+        if (gameFrame3 === random3){
+            cancelAnimationFrame(gameFrame3);
+            console.log(roller3frame);
+            rollerFrames.push(roller3frame)
+            x++;
         }
-        if (x == 4){
-            checkRollers()
+    }
+}
+function animateRoller4(){
+    contex.drawImage(roller1, 0 * spriteW, roller4frame * spriteH, spriteW, spriteH, 300, 250, spriteW, spriteH);
+    if(gameFrame4 < random4){
+        if (gameFrame4 % staggerFrames == 0){
+            if (roller4frame < 14) roller4frame++;
+            else roller4frame = 0;
         }
-
+        gameFrame4++;
+        if (gameFrame4 === random4){
+            cancelAnimationFrame(gameFrame4);
+            console.log(roller4frame);
+            rollerFrames.push(roller4frame)
+            x++;
+        }
     }
 }
 
 function checkRollers(){
-    if(rollerFrames.includes(roller1frame && roller2frame)){
-        console.log('jee')
+    console.log('checking..')
+    if(cherry.includes(rollerFrames) && cherry.includes(rollerFrames) && cherry.includes(rollerFrames)){
+        console.log('kirsikka')
+    }
+    if(pear.includes(roller1frame) && pear.includes(roller2frame) && pear.includes(roller3frame)){
+        console.log('päärynä')
     }
 }
